d759384cc7c6817c777d5020068dbc680c10dc6b
johnson <johnsonoye34@gmail.com>
2017-12-10 18:53:03 +0100

    i am happy

--- findit/aliexpress.py
+++ findit/aliexpress.py
@@ -142,32 +142,32 @@ def alii():
 				htl = Request(image,headers=hdr)
 				httl = urlopen(htl).read()
 
-		if Products.objects.filter(name=namelst,shop='aliexpress').exists():
-			produc = Products.objects.get(name=namelst,shop='aliexpress')
-			# Checks the price
-			if produc.price != product_price:
-				produc.old_price = produc.price
-				produc.old_price_digit = int(produc.price.replace(',','').replace('\n','').replace('.00',''))
-				# Updates the price
-				produc.price = product_price
-				# Saves the price
-				
-				produc.save()
-		else:
-			request = requests.get(image, stream=True)
-			if request.status_code != requests.codes.ok:
-				continue
-			randd_ne = get_random_string(length=10)
-			file_name = image.split('/')[-1]
-			point_finder = file_name.find('.')
-			file_name = file_name[:point_finder] + randd_ne
-			lf = tempfile.NamedTemporaryFile()
-			for block in request.iter_content(1024*8):
-				if not block:
-					break
-				lf.write(block)
-			lf = ContentFile(httl)
-			product = Products(name=namelst,price=product_price,source_url=product_link,shop='aliexpress',genre='headphones')
-			product.image.save(file_name[:10],lf)
-			print('done')
+			if Products.objects.filter(name=namelst,shop='aliexpress').exists():
+				produc = Products.objects.get(name=namelst,shop='aliexpress')
+				# Checks the price
+				if produc.price != product_price:
+					produc.old_price = produc.price
+					produc.old_price_digit = int(produc.price.replace(',','').replace('\n','').replace('.00',''))
+					# Updates the price
+					produc.price = product_price
+					# Saves the price
+					
+					produc.save()
+			else:
+				request = requests.get(image, stream=True)
+				if request.status_code != requests.codes.ok:
+					continue
+				randd_ne = get_random_string(length=10)
+				file_name = image.split('/')[-1]
+				point_finder = file_name.find('.')
+				file_name = file_name[:point_finder] + randd_ne
+				lf = tempfile.NamedTemporaryFile()
+				for block in request.iter_content(1024*8):
+					if not block:
+						break
+					lf.write(block)
+				lf = ContentFile(httl)
+				product = Products(name=namelst,price=product_price,source_url=product_link,shop='aliexpress',genre='headphones')
+				product.image.save(file_name[:10],lf)
+				print('done')
 
