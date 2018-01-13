ee9ae27ad23a6b7015e5c2620a301caf0504cdba
oyejohnson <johnsonoye34@gmail.com>
2018-01-13 14:37:30 +0100

    llkkkkkfkfkfe

--- findit/views.py
+++ findit/views.py
@@ -711,7 +711,7 @@ def feedback(request):
 
 
 def despiration(request):
-    stuffs = Products.objects.all()
+    stuffs = Products.objects.all().exclude(genre='aliexpress')
     for i in stuffs:
         i.real_price = int(i.price.replace(',', '').replace('\n', '').replace('.00', ''))
         i.save()
