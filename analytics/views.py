ecca09b75d8b0f821a431e9558c30b1f36699fb4
oyejohnson <johnsonoye34@gmail.com>
2018-01-13 02:02:50 +0100

    llkkkfjnn

--- analytics/views.py
+++ analytics/views.py
@@ -28,7 +28,7 @@ def usergrowth(request):
     data_sett = []
     dayses = []
     user_g = UserNumber.objects.extra({'date_added': "date(date_added)"}).values('date_added').annotate(
-        date_added_count=Count('id')).order_by('date_added')
+        date_added_count=Count('id')).order_by('date_added')[10:]
     for user in user_g:
         data_sett.append(user['date_added_count'])
         dayses.append(datetime.datetime.strptime(str(user['date_added']), '%Y-%m-%d').strftime('%a'))
@@ -39,7 +39,7 @@ def userClicks(request):
     data_sett = []
     dayses = []
     user_g = ObjectViewed.objects.extra({'timestamp': "date(timestamp)"}).values('timestamp').annotate(
-        date_added_count=Count('id')).order_by('timestamp')
+        date_added_count=Count('id')).order_by('timestamp')[10:]
     for user in user_g:
         data_sett.append(user['date_added_count'])
         dayses.append(datetime.datetime.strptime(str(user['timestamp']), '%Y-%m-%d').strftime('%a'))
