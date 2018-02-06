0970f02778bdd216ea04f8bfe3e28b9f8c2267d3
oyejohnson <johnsonoye34@gmail.com>
2018-02-06 22:29:39 +0100

    lllfkkfkfkf

--- findit/views.py
+++ findit/views.py
@@ -131,8 +131,8 @@ def real_index(request):
     # landlord(request,ad)
     # seen_by(request,prod_ad)
     # landlord(request,prod_ad)
-    user_c_name, user_c_code = get_location(request=request)
-    user_count(request)
+    #user_c_name, user_c_code = get_location(request=request)
+    #user_count(request)
     share_string = 'Quickfinda - Online Shop & Price Comparison in Nigeria'
     t1 = time.time()
     url = request.build_absolute_uri()
