95bf9fe6ad6255d0207d6cc2c7716ebfa74cfb00
oyejohnson <johnsonoye34@gmail.com>
2018-02-07 16:26:06 +0100

    west wood border

--- findit/views.py
+++ findit/views.py
@@ -64,6 +64,95 @@ def going_global(request):
     return HttpResponse("Bigest Fan")
 
 
+# Search Plugin
+def search_bite(request,query):
+    all_products = Products.objects.order_by('?')
+    whichPage(request, 'search', request.build_absolute_uri())
+    if 'iphone' in str(query.lower()) or 'ipad' in str(query.lower()):
+        # # print(query)
+        # print(list(query))
+        query = query.lower()
+        quey = query.split()
+
+        for stop_w in STOP_WORDS:
+            if stop_w in quey:
+                quey.remove(stop_w)
+
+        if len(quey) >= 3:
+            if 'plus' in quey and len(quey) <= 3:
+                q = ' '.join(quey)
+                all_products = all_products.filter(
+                    Q(name__icontains=q) |
+                    Q(name__iexact=q)
+                ).distinct()
+            else:
+                for q in quey:
+                    all_products = all_products.filter(
+                        Q(name__icontains=q)
+
+                    ).distinct()
+
+            add_query(query, 
+                'search page', 
+                 all_products[:10], 
+                 nbool=True, 
+                 correct=query, 
+                 request=request)
+        else:
+            # query = correction(query)
+
+            # print(query)
+            query = query.strip()
+            query = query.split()
+            for stop_w in STOP_WORDS:
+                if stop_w in query:
+                    query.remove(stop_w)
+
+            query = ' '.join(query)
+            print(query)
+            all_products = all_products.filter(
+                Q(name__icontains=query) |
+                Q(name__iexact=query)
+            ).distinct()
+            if len(all_products) == 0:
+                add_query(query, 
+                    'search page', 
+                    all_products[:10], 
+                    nbool=False, 
+                    correct=query, 
+                    request=request)
+            else:
+                add_query(query, 
+                    'search page', 
+                    all_products[:10], 
+                    nbool=True, correct=query, request=request)
+    else:
+        query = query.split()
+        new = []
+        for stop_w in STOP_WORDS:
+            if stop_w in query:
+                query.remove(stop_w)
+        for q in query:
+            # q = correction(q)
+
+            # print(q)
+            new.append(q)
+            # Put them all together
+
+            all_products = all_products.filter(
+                Q(name__icontains=q) |
+                Q(name__iexact=q)
+            ).distinct()
+
+        query = ' '.join(query)
+        made = ' '.join(new)
+        if len(all_products) == 0:
+            add_query(query, 'search page', all_products[:10], nbool=False, correct=made, request=request)
+        else:
+            add_query(query, 'search page', all_products[:10], nbool=True, correct=made, request=request)
+
+    return all_products
+
 # Dark and light design
 def user_choice(request):
     user = get_client_ip(request)
@@ -151,76 +240,7 @@ def real_index(request):
         all_products = Products.objects.filter(genre__in=user_pick_list)
         all_products = all_products.order_by('?')
     if query:
-        all_products = Products.objects.order_by('?')
-        whichPage(request, 'search', request.build_absolute_uri())
-        if 'iphone' in str(query.lower()) or 'ipad' in str(query.lower()):
-            # # print(query)
-            # print(list(query))
-            query = query.lower()
-            quey = query.split()
-
-            for stop_w in STOP_WORDS:
-                if stop_w in quey:
-                    quey.remove(stop_w)
-
-            if len(quey) >= 3:
-                if 'plus' in quey and len(quey) <= 3:
-                    q = ' '.join(quey)
-                    all_products = all_products.filter(
-                        Q(name__icontains=q) |
-                        Q(name__iexact=q)
-                    ).distinct()
-                else:
-                    for q in quey:
-                        all_products = all_products.filter(
-                            Q(name__icontains=q)
-
-                        ).distinct()
-                add_query(query, 'search page', all_products[:10], nbool=True, correct=query, request=request)
-            else:
-                # query = correction(query)
-
-                # print(query)
-                query = query.strip()
-                query = query.split()
-                for stop_w in STOP_WORDS:
-                    if stop_w in query:
-                        query.remove(stop_w)
-
-                query = ' '.join(query)
-                print(query)
-                all_products = all_products.filter(
-                    Q(name__icontains=query) |
-                    Q(name__iexact=query)
-                ).distinct()
-                if len(all_products) == 0:
-                    add_query(query, 'search page', all_products[:10], nbool=False, correct=query, request=request)
-                else:
-                    add_query(query, 'search page', all_products[:10], nbool=True, correct=query, request=request)
-        else:
-            query = query.split()
-            new = []
-            for stop_w in STOP_WORDS:
-                if stop_w in query:
-                    query.remove(stop_w)
-            for q in query:
-                # q = correction(q)
-
-                # print(q)
-                new.append(q)
-                # Put them all together
-
-                all_products = all_products.filter(
-                    Q(name__icontains=q) |
-                    Q(name__iexact=q)
-                ).distinct()
-
-            query = ' '.join(query)
-            made = ' '.join(new)
-            if len(all_products) == 0:
-                add_query(query, 'search page', all_products[:10], nbool=False, correct=made, request=request)
-            else:
-                add_query(query, 'search page', all_products[:10], nbool=True, correct=made, request=request)
+        all_products = search_bite(request,query)
     # if corrected_sentence != orginal_sentence:
     # 	corrected_sentence = ' '.join(corrected_sentence)
     # 	orginal_sentence = ' '.join(orginal_sentence)
