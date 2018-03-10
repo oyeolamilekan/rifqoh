23abdc9c9a3e6b92843745158d2a8905c1c6e57f
oyejohnson <johnsonoye34@gmail.com>
2018-03-10 23:43:41 +0100

    made url form disabled

--- findit/forms.py
+++ findit/forms.py
@@ -6,5 +6,5 @@ class feedBackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ('feelings', 'url_locator', 'email', 'content')
-        labels = {'content': 'body', 'feelings': 'How was the services', 'url_locator': 'Current location'}
-        widgets = {'content': forms.Textarea(attrs={'cols': 10, 'rows': 3})}
+        labels = {'content': 'body', 'feelings': 'How was the services', 'url_locator': 'Current Page'}
+        widgets = {'content': forms.Textarea(attrs={'cols': 10, 'rows': 3}),'url_locator': forms.TextInput(attrs={'disabled': True}),}
