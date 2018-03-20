05a9c80abd51bc6bf582fa527f8c50e39702c35e
oyejohnson <johnsonoye34@gmail.com>
2018-03-20 10:30:13 +0100

    addedp it inlllwkl

--- pixxelated/urls.py
+++ pixxelated/urls.py
@@ -20,10 +20,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^you_are_stupid/', admin.site.urls),
+    url(r'^',include('findit.urls',namespace='findit')),
     url(r'^api/', include('api.urls', namespace='api')),
     url(r'^shop/',include('shop.urls',namespace='shop')),
     url(r'^double/',include('analytics.urls',namespace='analytics')),
-    url(r'^',include('findit.urls',namespace='findit')),
     url(r'^accounts/',include('accounts.urls')),
     url(r'^adengine/',include('adengine.urls',namespace='adengine'))
 ]
