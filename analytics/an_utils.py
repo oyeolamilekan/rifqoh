919d918c8ed8735c7ee543e4b85c0f7839d89cb8
oyejohnson <johnsonoye34@gmail.com>
2018-04-08 14:22:28 +0100

    bot list

--- analytics/an_utils.py
+++ analytics/an_utils.py
@@ -33,7 +33,7 @@ def is_bot(request):
     botnames = ('Googlebot', 'Slurp', 'Twiceler', 'msnbot', 'KaloogaBot', 'YodaoBot', 'Baiduspider', 'googlebot',
                 'Speedy Spider', 'DotBot','robot','bots','Mediapartners-Google','robot','Python-urllib',
                 'python-requests','YandexBot','Twitterbot','Trident','LinkedInBot','muhstik','OpenLinkProfiler.org',
-                'bingbot','DuckDuckGo','Trident','libwww-perl','zgrab')
+                'bingbot','DuckDuckGo','Trident','libwww-perl','zgrab','SemrushBot','bot','Bot')
 
     user_agent = request.META.get('HTTP_USER_AGENT', None)
     tested = None
